e3f63c37777c9f83b26ed96603a1d6c6e74124b0
Shashwat Tiwari <tshashwat25@gmail.com>
2020-09-06 19:14:43 +0530

    changes

--- public/js/app.js
+++ public/js/app.js
@@ -3,15 +3,9 @@ window.addEventListener('load', () => {
 
   // Compile Handlebar Templates
   const errorTemplate = Handlebars.compile($('#error-template').html());
-  const ratesTemplate = Handlebars.compile($('#rates-template').html());
-  const exchangeTemplate = Handlebars.compile($('#exchange-template').html());
-  const historicalTemplate = Handlebars.compile($('#historical-template').html());
-
-  // Instantiate api handler
-  const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    timeout: 5000,
-  });
+  const homeTemplate = Handlebars.compile($('#home-template').html());
+  const shopTemplate = Handlebars.compile($('#shop-template').html());
+  const magazineTemplate = Handlebars.compile($('#magazine-template').html());
 
   const router = new Router({
     mode: 'history',
@@ -32,126 +26,56 @@ window.addEventListener('load', () => {
     el.html(html);
   };
 
-  // Display Latest Currency Rates
+  // Display Home
   router.add('/', async () => {
     // Display loader first
-    let html = ratesTemplate();
+    let html = homeTemplate();
     el.html(html);
-    try {
-      // Load Currency Rates
-      const response = await api.get('/rates');
-      const { base, date, rates } = response.data;
-      // Display Rates Table
-      html = ratesTemplate({ base, date, rates });
-      el.html(html);
-      $('.loading').removeClass('loading');
-    } catch (error) {
-      showError(error);
-    }
+    $('li').click(filterBy);
+     loadDta('');
+    $('.loading').removeClass('loading');
   });
+  
+  // filter methods
+  const filterBy =($event) =>{
+    $(".container2").empty();
+    loadDta($event.target.innerText);
+  }
+  
+  // display products on home page
+  const loadDta = (val) =>{
+    var flickerAPI = $.getJSON( "api.json", function() {
+      console.log( "success" );
+    })
+   .done(function( data ) {
+     var container = $("<div>").addClass("container2").appendTo("#images");
+     let dataIterate = data.filter(function (e) {
+      return val == '' || val == e.category;
+    });
+     for(var i = 0; i < dataIterate.length; i++) {
+       blockDiv = $("<div>").addClass("iblock").appendTo(container);
+       $( "<img>" ).attr( "src", dataIterate[i].src ).appendTo(blockDiv);
+       blocktextDiv = $("<div>").addClass("block").appendTo(blockDiv);
+       $('<p>').text(dataIterate[i].title).appendTo(blocktextDiv);
+       $( "<span>" ).text("$" + dataIterate[i].price +"").appendTo(blocktextDiv);
+       $('<p>').addClass("category").text(dataIterate[i].category).appendTo(blockDiv);
+       $( "<img>" ).addClass("cart").attr( "src", "cart.jpg" ).appendTo(blockDiv);
+     }
+   });
+  }
 
-  // Perform POST request, calculate and display conversion results
-  const getConversionResults = async () => {
-    // Extract form data
-    const from = $('#from').val();
-    const to = $('#to').val();
-    const amount = $('#amount').val();
-    // Send post data to express(proxy) server
-    try {
-      const response = await api.post('/convert', { from, to });
-      const { rate } = response.data;
-      const result = rate * amount;
-      $('#result').html(`${to} ${result}`);
-    } catch (error) {
-      showError(error);
-    } finally {
-      $('#result-segment').removeClass('loading');
-    }
-  };
-
-  // Handle Convert Button Click Event
-  const convertRatesHandler = () => {
-    if ($('.ui.form').form('is valid')) {
-      // hide error message
-      $('.ui.error.message').hide();
-      // Post to express server
-      $('#result-segment').addClass('loading');
-      getConversionResults();
-      // Prevent page from submitting to server
-      return false;
-    }
-    return true;
-  };
-
-  router.add('/exchange', async () => {
+  router.add('/shop', async () => {
     // Display loader first
-    let html = exchangeTemplate();
+    let html = shopTemplate();
     el.html(html);
-    try {
-      // Load Symbols
-      const response = await api.get('/symbols');
-      const { symbols } = response.data;
-      html = exchangeTemplate({ symbols });
-      el.html(html);
-      $('.loading').removeClass('loading');
-      // Specify Form Validation Rules
-      $('.ui.form').form({
-        fields: {
-          from: 'empty',
-          to: 'empty',
-          amount: 'decimal',
-        },
-      });
-      // Specify Submit Handler
-      $('.submit').click(convertRatesHandler);
-    } catch (error) {
-      showError(error);
-    }
+    //  loader hidden
+    $('.loading').removeClass('loading');
   });
 
-  const getHistoricalRates = async () => {
-    const date = $('#date').val();
-    try {
-      const response = await api.post('/historical', { date });
-      const { base, rates } = response.data;
-      const html = ratesTemplate({ base, date, rates });
-      $('#historical-table').html(html);
-    } catch (error) {
-      showError(error);
-    } finally {
-      $('.segment').removeClass('loading');
-    }
-  };
-
-  const historicalRatesHandler = () => {
-    if ($('.ui.form').form('is valid')) {
-      // hide error message
-      $('.ui.error.message').hide();
-      // Indicate loading status
-      $('.segment').addClass('loading');
-      getHistoricalRates();
-      // Prevent page from submitting to server
-      return false;
-    }
-    return true;
-  };
-
-  router.add('/historical', () => {
-    const html = historicalTemplate();
+  router.add('/magazine', () => {
+    const html = magazineTemplate();
     el.html(html);
-    // Activate Date Picker
-    $('#calendar').calendar({
-      type: 'date',
-      formatter: {
-        date: date => new Date(date).toISOString().split('T')[0],
-      },
-    });
-    $('.ui.form').form({
-      fields: {
-        date: 'empty',
-      },
-    });
-    $('.submit').click(historicalRatesHandler);
+
   });
 
   router.navigateTo(window.location.pathname);
